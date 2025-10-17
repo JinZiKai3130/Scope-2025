61684618b4bd4ecc9ce2672c9b37c9455dec671a
ZiKai <jinzikai@sjtu.edu.cn>
2025-10-17 14:58:00 +0800

    P1383 & part of Michelle

--- Michelle/Michelle.cpp
+++ Michelle/Michelle.cpp
@@ -1,10 +1,57 @@
 #include <iostream>
-using namespace std;
+#include <cstring>
+#include <unordered_map>
+#include <set>
+struct Students {
+    std::string name;
+    bool gender;
+    int _class, score[10], avg;
+
+    bool operator<(const Students& other) const {
+        if (avg != other.avg) {
+            return avg > other.avg;
+        }
+        return name < other.name;
+    }
+};
+
+std::unordered_map<std::string, int> m;
+std::set<Students> data;
+bool flag = 0;
+
+
+void Add() {
+    Students curr;
+    std::cin >> curr.name >> curr.gender >> curr._class;
+    for (int i = 0; i < 9; i++) {
+        std::cin >> curr.score[i];
+    }
+    if (flag) {
+        std::cout << "[Error]Cannot add student now." << '\n';
+        return;
+    }
+    if (m.find(curr.name) != m.end()) {
+        std::cout << "[Error]Add failed." << '\n';
+        return;
+    }
+    m[curr.name] = 0;
+    data.insert(curr);
+}
 
 
 int main() {
-    int n;
-
-
+    std::string oper;
+    while(1) {
+        std::cin >> oper;
+        if (oper == "ADD") Add();
+        if (oper == "START") flag = 1, Flush();
+        if (oper == "UPDATE") Update();
+        if (oper == "FLUSH") Flush();
+        if (oper == "PRINTLIST") Printlist();
+        if (oper == "QUERY") Query();
+        if (oper == "END") break;
+    }
     return 0;
 }
+
+// 是否可能没有START
--- P1383.cpp
+++ P1383.cpp
@@ -1,10 +1,165 @@
 #include <iostream>
-using namespace std;
+#include <map>
+#include <vector>
+#include <set>
+#include <cstring>
+struct Activity {
+    std::string str;
+    int avg, level;
 
+    bool operator<(const Activity& other) const {
+        if (avg != other.avg) {
+            return avg > other.avg;
+        }
+        if (level != other.level) {
+            return level < other.level;
+        }
+        return str < other.str;
+    }
+};
+
+std::map<std::string, std::vector<int>> m;
+std::set<Activity> s;
+std::string type, oper;
+
+void mapInsert() {
+    std::string name;
+    int num, x;
+    std::cin >> name >> num;
+    if (m.find(name) != m.end()) {
+        for(int i = 0; i < num; i++) {
+            std::cin >> x;
+        }
+        std::cout << "Error" << std::endl;
+    }
+    else for(int i = 0; i < num; i++) {
+        std::cin >> x;
+        m[name].push_back(x);
+    }
+}
+
+void mapTraverse() {
+    for (auto it = m.begin(); it != m.end(); it++) {
+        std::cout << it->first << " ";
+        for (int i = 0; i < it->second.size(); i++) {
+            std::cout << it->second[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void mapSize() {
+    std::cout << m.size() << std::endl;
+}
+
+void mapErase() {
+    // std::cout << "erase" << std::endl;
+    std::string name;
+    std::cin >> name;
+    if (m.find(name) == m.end()) std::cout << "Error" << std::endl;
+    else m.erase(name);
+}
+
+void mapCount() {
+    std::string name;
+    std::cin >> name;
+    std::cout << m.count(name) << std::endl;
+}
+
+void mapVisitVec() {
+    std::string name;
+    int index;
+    std::cin >> name >> index;
+    if (m.find(name) == m.end()) {
+        std::cout << "Error" << std::endl;
+        return;
+    }
+    if (m[name].size() <= index || index < 0) {
+        std::cout << "Error" << std::endl;
+        return;
+    }
+    std::cout << m[name][index] << std::endl;
+}
+
+void mapVisit() {
+    std::string name;
+    std::cin >> name;
+    if (m.find(name) == m.end()) {
+        std::cout << "Error" << std::endl;
+        return;
+    }
+    int len = m[name].size();
+    std::cout << len << " ";
+    for (int i = 0; i < len; i++) {
+        std::cout << m[name][i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+void mapAppend() {
+    std::string name;
+    int element;
+    std::cin >> name >> element;
+    if (m.find(name) == m.end()) {
+        std::cout << "Error" << std::endl;
+        return;
+    }
+    m[name].push_back(element);
+}
+
+void setSize() {
+    std::cout << s.size() << std::endl;
+}
+
+void setInsert() {
+    Activity x;
+    std::cin >> x.avg >> x.level >> x.str;
+    if (s.find(x) == s.end()) s.insert(x);
+    else std::cout << "Error" << std::endl;
+}
+
+void setErase() {
+    Activity x;
+    std::cin >> x.avg >> x.level >> x.str;
+    if(s.find(x) == s.end()) std::cout << "Error" << std::endl;
+    else s.erase(x);
+}
+
+void setCount() {
+    Activity x;
+    std::cin >> x.avg >> x.level >> x.str;
+    std::cout << s.count(x) << std::endl;
+}
+
+void setTraverse() {
+    for (auto it = s.begin(); it != s.end(); it++) {
+        std::cout << it->avg << " " << it->level << " " << it->str << std::endl;
+    }
+}
 
 int main() {
-    int n;
-
-
+    // std::freopen("P1383.out", "w", stdout);
+    while(1) {
+        std::cin >> type;
+        if (type == "end") break;
+        std::cin >> oper;
+        if (type == "map") {
+            if(oper == "insert") mapInsert();
+            if(oper == "traverse") mapTraverse();
+            if(oper == "size") mapSize();
+            if(oper == "erase") mapErase();
+            if(oper == "count") mapCount();
+            if(oper == "append") mapAppend();
+            if(oper == "visit") mapVisit();
+            if(oper == "visitVec") mapVisitVec();
+        }
+        if (type == "set") {
+            if(oper == "insert") setInsert();
+            if(oper == "traverse") setTraverse();
+            if(oper == "size") setSize();
+            if(oper == "erase") setErase();
+            if(oper == "count") setCount();
+        }
+    }
     return 0;
 }
